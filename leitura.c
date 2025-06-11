bffd834300f3fe3a90d3955f6b10542dcb5fe1da
hugostoso1 <hugoaasouza45@gmail.com>
2025-06-11 12:32:23 -0300

    pdfk

--- leitura.c
+++ leitura.c
@@ -1,46 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "fila_addr.h"
 
-int contarLinhas(char *arquivo){
-    FILE *arquivo = fopen(arquivo, "r");
-    if(arquivo == NULL){
-        printf("Erro ao abrir o arquivo");
-        return -1;
-    }
-
-    int linhas = 0;
-    int ch;
-
-    while((ch = fgetc(arquivo)) != EOF){
-        if (ch == '\n') {
-            linhas++;
-        }
-    }
-
-    fclose(arquivo);
-    return linhas;
-}
 
 int hex_int(const char *hex){
     return (int)strtol(hex, NULL, 16);
 }
 
-int leituraArquivo(char *arquivo){
-    int linhas = contarLinhas(arquivo);
-
-    FILE *arquivo = fopen(arquivo, "r");
+Fila_addr* leituraArquivo(char *path){
+    FILE *arquivo = fopen(path, "r");
     if(arquivo == NULL){
         printf("Erro ao abrir o arquivo");
         return -1;
     }
 
-    int *dados = (int*) malloc (sizeof(int) * linhas);
+    Fila_addr *fila = criaFilaAddr();
 
-    // char *token = strtok(arquivo, " ");
-    // for(int i = 0; i < linhas; i++){
-    //     dados[0] = 
+
+    // char *tok1;
+    // char *addrs = strtok_r(arquivo, "\n", &tok1);
+    // while(addrs != NULL){
+    //     char *tok2;
+    //     char *aux = strtok_r(addrs, " ", &tok2);
+
+    //     char *addrsFile = (char*) malloc (strlen(aux) + 1);
+
+    //     strcpy(addrsFile, aux);
+    //     aux = strtok_r(NULL, )
+
 
     // }
 
+    char *addrs = strtok(arquivo, " \n");
+    while(addrs != NULL){
+        char *addrsFile = (char*) malloc (strlen(addrs) + 1);
+
+        strcpy(addrsFile, addrs);
+
+        addrs = strtok(NULL, " \n");
+        
+        char opFile = addrs[0];
+
+        enfileirarAddr(fila, addrsFile, opFile);
+    }
+
+    return fila; 
 
 }
+
+
+    // char *tokPipe;
+
+    // char *comando = strtok_r(entrada, "|", &tokPipe);
+    // while(comando != NULL){
+    //     int tam = 10;
+    //     char **args_parcial = malloc(sizeof(char*) * tam);
+    //     if(args_parcial == NULL) exit(EXIT_FAILURE);
+    //     int i = 0;
+
+    //     char *token;
+    //     char *tokInterno;
+    //     token = strtok_r(comando, " \t\n\r", &tokInterno);
+    //     while(token != NULL){
+    //         if(i >= tam){  // se a quantidade de argumentos superar a quantidade de linhas, realoca a matriz de argumentos
+    //             tam *= 2;
+    //             args_parcial = realloc(args_parcial, tam * sizeof(char*));
+    //             if(args_parcial == NULL) exit(EXIT_FAILURE);
+    //         }
+    //         args_parcial[i++] = strdup(token);
+    //         token = strtok_r(NULL, " \t\n\r", &tokInterno);
+    //     }
+
+    //     args_parcial[i] = NULL;
+    //     enfileirarArgs(saida, args_parcial);
+
+    //     comando = strtok_r(NULL, "|", &tokPipe);
+    // }
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "fila_addr.h"
+#include "leitura.h"
 
 int main(int argc, char *argv[]){
+    Fila_addr = leitura
     
 
 }

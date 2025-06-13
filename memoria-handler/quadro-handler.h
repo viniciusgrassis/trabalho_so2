7849101dd2807ef446dcdf85f9ebd138933c966b
hugostoso1 <hugoaasouza45@gmail.com>
2025-06-12 22:40:52 -0300

    wpiddvnwpkn

---
+++ memoria-handler/quadro-handler.h
@@ -0,0 +1,10 @@
+#ifndef QUADRO_HANDLER_h
+#define QUADRO_HANDLER_h
+
+typedef struct{
+    int *quadroPosicoes; // vetor dos quadros de paginas - bianry boy
+    int posicoesPreenchidas; // 
+    int tamanhoTotal; // total
+} quadrosPagina;
+
+#endif

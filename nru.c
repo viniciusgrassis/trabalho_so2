2a1e2a6ace5abf83460478e4c71e5295b9ecaee8
Vinicius Assis <vini1904@hotmail.com>
2025-06-13 22:41:16 -0300

    nru

---
+++ nru.c
@@ -0,0 +1,72 @@
+#include "./estrutura-de-dados/hash.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+PaginaVirtual* encontrarVitimaNRU(Hash* tabelaPaginas){
+    PaginaVirtual* vitima = NULL;
+    for(int i = 0; i < tabelaPaginas->tamanho; i++){
+        FilaPagVirtual* fila = tabelaPaginas->tabela[i];
+        if(fila == NULL || fila->inicio == NULL) continue;
+        PaginaVirtual* pagina = fila->inicio;
+        while(pagina != NULL){
+            if(pagina->referenciada == 0 && pagina->modficada == 0){ // classe 0
+                vitima = pagina;
+                break;
+            }
+            pagina = pagina->prox;
+        }
+        if(vitima != NULL) break;
+
+    }
+    if(vitima == NULL){
+        for(int i = 0; i < tabelaPaginas->tamanho; i++){
+            FilaPagVirtual* fila = tabelaPaginas->tabela[i];
+            if(fila == NULL || fila->inicio == NULL) continue;
+            PaginaVirtual* pagina = fila->inicio;
+            while(pagina != NULL){
+                if(pagina->referenciada == 0 && pagina->modficada == 1){ // classe 1
+                    vitima = pagina;
+                    break;
+                }
+                pagina = pagina->prox;
+            }
+            if(vitima != NULL) break;
+        }
+    }
+    if(vitima == NULL){
+        for(int i = 0; i < tabelaPaginas->tamanho; i++){
+            FilaPagVirtual* fila = tabelaPaginas->tabela[i];
+            if(fila == NULL || fila->inicio == NULL) continue;
+            PaginaVirtual* pagina = fila->inicio;
+            while(pagina != NULL){
+                if(pagina->referenciada == 1 && pagina->modficada == 0){ // classe 2
+                    vitima = pagina;
+                    break;
+                }
+                pagina = pagina->prox;
+            }
+            if(vitima != NULL) break;
+        }
+    }
+
+    if(vitima == NULL){
+        for(int i = 0; i < tabelaPaginas->tamanho; i++){
+            FilaPagVirtual* fila = tabelaPaginas->tabela[i];
+            if(fila == NULL || fila->inicio == NULL) continue;
+            PaginaVirtual* pagina = fila->inicio;
+            while(pagina != NULL){
+                if(pagina->referenciada == 1 && pagina->modficada == 1){  // classe 3
+                    vitima = pagina;
+                    break;
+                }
+                pagina = pagina->prox;
+            }
+            if(vitima != NULL) break;
+        }
+    }
+    if(vitima == NULL){
+        return NULL;
+    }
+}
+
